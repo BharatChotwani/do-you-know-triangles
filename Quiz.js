abd4bfd8682351200afcd96c036f3c330e574052
Bharat <bchotwani3@gmail.com>
2021-09-12 19:08:24 +0530

    first commit

---
+++ Quiz.js
@@ -0,0 +1,5 @@
+const quizForm = document.querySelector(".quiz-form");
+const submitBtn = document.querySelector("#submit-btn");
+const outputEl = document.querySelector("#show-output");
+
+const correctAnswers = ["90°", "right angled"];
