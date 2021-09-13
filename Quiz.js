493f56d986ec0f0abdb895a93d946f362c6a0d5c
Bharat <bchotwani3@gmail.com>
2021-09-13 19:07:51 +0530

    Update JS

--- Quiz.js
+++ Quiz.js
@@ -3,3 +3,19 @@ const submitBtn = document.querySelector("#submit-btn");
 const outputEl = document.querySelector("#show-output");
 
 const correctAnswers = ["90°", "right angled"];
+
+function calculateScore() {
+    let score = 0;
+    let index = 0;
+    const formResults = new FormData(quizForm);
+    for(let value of formResults.values()){
+        if(value === correctAnswers[index]){
+            score = score + 1;
+        }
+        index = index + 1;
+    }
+    // console.log(score);
+    outputEl.innerText = "Your score is" + score;
+}
+
+submitBtn.addEventListener("click", calculateScore);
