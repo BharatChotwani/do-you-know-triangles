b24d1cc306aee3b4ca87247bd6cda0d4f245011d
Bharat <bchotwani3@gmail.com>
2021-09-13 19:41:31 +0530

    first commit

---
+++ CalHypotenuse.js
@@ -0,0 +1,18 @@
+// .side-input , #calculate-hypotenuse , #show-output
+const inputs = document.querySelectorAll(".side-input");
+const hypotenuseBtn = document.querySelector("#calculate-hypotenuse");
+const outPut = document.querySelector("#show-output");
+
+function calculateSqrt(a, b){
+    const sideSqrt = a*a + b*b ;
+    console.log(sideSqrt);
+    return sideSqrt
+}
+
+function calculateHypotenuse() {
+   const sideSqrt = calculateSqrt(Number(inputs[0].value), Number(inputs[1].value));
+   const lengthOfHypotenuse = Math.sqrt(sideSqrt);
+   console.log(lengthOfHypotenuse);
+}
+
+hypotenuseBtn.addEventListener("click", calculateHypotenuse);
