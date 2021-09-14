e02b0b912b90673850c26d002e9d28c978c246a2
Bharat <bchotwani3@gmail.com>
2021-09-14 13:16:23 +0530

    second commit

--- Area.js
+++ Area.js
@@ -3,8 +3,14 @@ const areaInputs = document.querySelectorAll(".area-inputs");
 const calculateAreaBtn = document.querySelector("#calculate-area");
 const showOutput = document.querySelector("#output");
 
+function calculateSides(a, b){
+    const sumOfSides = (a * b / 2);
+    return sumOfSides;
+}
+
 function calcculateArea(){
-    
+    const sumOfSides = calculateSides(Number(areaInputs[0].value), Number(areaInputs[1].value));
+    showOutput.innerText = "The area of Triangle is " + sumOfSides;
 }
 
 calculateAreaBtn.addEventListener("click", calcculateArea);
