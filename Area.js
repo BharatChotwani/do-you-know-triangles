1a4fcac165c3a84e7e5df154859dff6905500e1a
Bharat <bchotwani3@gmail.com>
2021-09-14 12:04:42 +0530

    first commit

---
+++ Area.js
@@ -0,0 +1,10 @@
+//.area-inputs , #calculate-area , #output
+const areaInputs = document.querySelectorAll(".area-inputs");
+const calculateAreaBtn = document.querySelector("#calculate-area");
+const showOutput = document.querySelector("#output");
+
+function calcculateArea(){
+    
+}
+
+calculateAreaBtn.addEventListener("click", calcculateArea);
